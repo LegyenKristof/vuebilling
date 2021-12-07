b05098397eab9796325373d1c10ac8ce35f35eb6
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2021-12-07 15:22:22 +0100

    Táblázat komponens elkészítve

---
+++ src/components/Tablazat.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <table>
+          <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Quantity</th>
+          </tr>
+          <tr v-for="sor in sorok" v-bind:key="sor.title">
+              <td v-for="adat in sor" v-bind:key="adat">{{adat}}</td>
+          </tr>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["sorok"]
+}
+</script>
